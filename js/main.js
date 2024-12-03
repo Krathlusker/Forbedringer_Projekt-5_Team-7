2bed2bc77d9cfa555cb2e2222738caf324e11313
minihauner <jannickhaunstrup@gmail.com>
2024-12-03 13:56:49 +0100

    tilføj første del af skelettet til header og footer samt en smule dertilhørende styling og js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* Funktionalitet til at åbne og lukke mobilmenu */
+
+const mobileNavToggle = document.getElementById('mobile-nav-toggle');
+const mobileNavClose = document.getElementById('mobile-nav-close');
+const mobileNav = document.getElementById('mobile-nav');
+
+mobileNavToggle.addEventListener('click', () => {
+    if (!mobileNav.classList.contains('main-nav-mobile--open')) {
+        mobileNav.classList.add('main-nav-mobile--open');
+        document.body.classList.add('body--no-scroll');
+    }
+});
+
+mobileNavClose.addEventListener('click', () => {
+    if (mobileNav.classList.contains('main-nav-mobile--open')) {
+        mobileNav.classList.remove('main-nav-mobile--open');
+        document.body.classList.remove('body--no-scroll');
+    }
+});
