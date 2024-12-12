21e6eae3997dfe1cd332a9b45a950b85820c1b69
AntonFraFilmfar <antonvja@gmail.com>
2024-12-13 00:41:41 +0100

    Alt tags, og opdaterede billeder
    Der er nu alt tags på alle billeder, samt er billederne blevet byttet ud med deres optimerede versioner.
    
    Der er også kommet en lille historie der udfolder sig gennem instagram opslagene. Glædelig jul!

--- js/Frederik.js
+++ js/Frederik.js
@@ -9,7 +9,7 @@ const events = [
         title: "Lasercutter-kursus",
         date: "19.1.2025",
         description: "Drømmer du også om at lave dine egne avancerede prototyper? Med et kursus i vores lasercuttere, åbner du op for mange muligheder for at gøre dine idéer til virkelighed.",
-        image: "./img/temp/277463923_2021874247973141_387521198919364496_n.jpg",
+        image: "./img/temp/277463923_2021874247973141_387521198919364496_n.webp",
     },
     {
         title: "Tekstiltryk-kursus",
@@ -21,13 +21,13 @@ const events = [
         title: "Design Thinking Workshop",
         date: "20.2.2025",
         description: "Har du en idé, men mangler du processen til at gøre den til virkelighed? På vores Design Thinking workshop lærer du en metode, der sikrer, at du udvikler brugerfokuserede og innovative løsninger. Kom og prøv kræfter med processen i praksis!",
-        image: "./img/temp/Wikimania_2024_-_GLAM_Global_Meetup_afternoon_03_-_Sustainable_Future_brainstorming.jpg",
+        image: "./img/temp/Wikimania_2024_-_GLAM_Global_Meetup_afternoon_03_-_Sustainable_Future_brainstorming.webp",
     },
     {
         title: "Illustrator for begyndere",
         date: "15.3.2025",
         description: "Vil du gerne lave dine egne flotte designs? På dette kursus lærer du de grundlæggende funktioner i Adobe Illustrator. Vi gennemgår værktøjer, teknikker og små tricks, så du kan skabe imponerende grafik til dine projekter.",
-        image: "./img/temp/41689670925_b8810ebdff_c.jpg",
+        image: "./img/temp/41689670925_b8810ebdff_c.webp",
     },
     {
         title: "Tinkercad Introduktion",
@@ -39,13 +39,13 @@ const events = [
         title: "Prusa XL Multitool Printer Workshop",
         date: "10.4.2025",
         description: "Oplev det nyeste inden for 3D-printteknologi med Prusa XL multitool printeren. Lær hvordan du kan kombinere forskellige materialer og teknikker i én enkelt printproces. En spændende mulighed for dem, der vil tage deres prototyper til næste niveau!",
-        image: "./img/machines/Original_Prusa_XL_spool.jpg",
+        image: "./img/machines/Original_Prusa_XL_spool.webp",
     },
     {
         title: "Elektronik og kredsløb",
         date: "18.6.2025",
         description: "Vil du gerne lære at bygge dine egne elektroniske kredsløb? På dette kursus tager vi dig igennem grundprincipperne i elektronik, fra lodning til opsætning af simple kredsløb. Perfekt til prototyping og gadgets.",
-        image: "./img/temp/Arduino-based_wow-and-flutter_meter_03.jpg",
+        image: "./img/temp/Arduino-based_wow-and-flutter_meter_03.webp",
     }
 ];
 
